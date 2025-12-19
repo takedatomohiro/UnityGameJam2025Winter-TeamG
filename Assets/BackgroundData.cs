03027acfd1113eac9ea456403811f827a14600ea
KawaharaTakagi <CA01254011@st.kawahara.ac.jp>
2025-12-19 13:33:19 +0900

    オプション追加

---
+++ Assets/BackgroundData.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BackgroundData
+{
+    private static string _selectedBackgroundName;
+
+    public static string selectedBackgroundName
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(_selectedBackgroundName))
+            {
+                _selectedBackgroundName = "natu"; // èââÒÇæÇØê›íËÅI
+            }
+            return _selectedBackgroundName;
+        }
+        set
+        {
+            _selectedBackgroundName = value;
+        }
+    }
+
+    public static readonly string[] availableBackgrounds = {
+        "natu",
+        "yoru",
+        "yuki",
+        "haru",
+        "bg_winter",
+        "bg_mountain"
+    };
+}
